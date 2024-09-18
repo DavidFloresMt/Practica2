4a6ac230c15f1c14f51fbc6f3fe5646ca3742bdf
David Flores <davidfloresm3009@gmail.com>
2024-09-17 19:45:12 -0600

    PrimerCommit

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int numeroAIterar = 2;
+int contadorDivisible = 0;
+
+while (numeroAIterar <= 100)
+{
+    for (int i = 1; i <= numeroAIterar; i++)
+    {
+        if(numeroAIterar%i == 0)
+            contadorDivisible++;
+
+        if(contadorDivisible > 2)
+            break;
+    }
+
+    if (contadorDivisible == 2)
+        Console.WriteLine($"El número {numeroAIterar} es primo");
+
+    contadorDivisible = 0;
+    numeroAIterar++;
+}
